686348f71ae4cb10f59dde58b67d6c806482ca02
Jake Gowler <jakecgowler@gmail.com>
2023-03-08 16:35:30 +0000

    et1_ccd_multiple_claimants syntax change

--- features/support/ccd_support/et1_ccd_multiple_claimants.rb
+++ features/support/ccd_support/et1_ccd_multiple_claimants.rb
@@ -66,10 +66,7 @@ module EtFullSystem
             end
 
             filenames = created_case.dig('case_fields', 'documentCollection').map {|doc| doc.dig('value', 'uploadedDocument', 'document_filename') }
-            expected_filenames = [
-              "et1_#{claimant.first.first_name.underscore}_#{claimant.first.last_name.underscore}.pdf",
-              "acas_#{respondents.first.name}.pdf"
-            ]
+            expected_filenames = %W[et1_#{claimant.first.first_name.underscore}_#{claimant.first.last_name.underscore}.pdf acas_#{respondents.first.name}.pdf]
             # The first respondent's acas is guaranteed to be in CCD but the other 4 (max) may come
             # later - and are not really important - so we ignore them if they are present
             respondents[1..4].each do |respondent|
