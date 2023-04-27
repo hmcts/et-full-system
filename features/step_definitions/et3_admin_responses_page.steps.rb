00de0841448c3e009bc2b764a2d41a42a1536590
Jake Gowler <jakecgowler@gmail.com>
2023-04-27 15:55:55 +0100

    syntax change

--- features/step_definitions/et3_admin_responses_page.steps.rb
+++ features/step_definitions/et3_admin_responses_page.steps.rb
@@ -4,7 +4,7 @@ end
 
 Then(/^I should see the ET3 responses page$/) do
   expect(page.current_url).to include("/admin/responses")
-  expect(admin_pages.responses_page).to be_displayed()
+  expect(admin_pages.responses_page).to be_displayed
 end
 
 When(/^I select the edit button for the first row and change the office$/) do
