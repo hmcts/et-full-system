baf372e1c3bedaa9e7c413a6e1d099650f9c8392
Jake Gowler <jakecgowler@gmail.com>
2023-06-13 13:09:01 +0100

    Cleanup

--- features/step_definitions/et1_group_claimants_steps.rb
+++ features/step_definitions/et1_group_claimants_steps.rb
@@ -31,7 +31,7 @@ Then("I should be able to submit two claimant details") do
   raise "Page not loaded within 1 second" unless has_loaded
   et1_group_claimants_page.set(@claimant)
   et1_group_claimants_page.save_and_continue
-  expect(et1_representatives_details_page).to have_page_header(wait: 0.05)
+  expect(et1_representatives_details_page).to have_page_header
 end
 
 When("there are group claimants") do
