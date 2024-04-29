a60e8414fc59e10d99947d69028e1c226bbd9b7e
Avi Singh <124276939+avi-singh-1912@users.noreply.github.com>
2024-04-29 15:01:52 +0100

    initial commit - tests to reflect new functionality as part of RST-6254 (#42)

--- features/et3/claiments_details_page.feature
+++ features/et3/claiments_details_page.feature
@@ -6,7 +6,7 @@ Background: ET3 claimants details page
   Given I am on the ET3 claimants details page
 
 Scenario: Verify Claimants details copy
-    Then Claimants details page copy texts are displayed in the correct language
+  Then Claimants details page copy texts are displayed in the correct language
 
 Scenario: Successfully submits all the claimants details
   When I successfully submit all the claimants details
@@ -21,3 +21,11 @@ Scenario: Dates given by the claimant optional validation
   But I do not provide the correct employment dates or give a reason
 #  Then I should see the error message saying the further information about dates cant be blank
   Then I should be taken to the earnings and benefits page
+
+Scenario: Invalid date format '0/0/0' provided
+  When I select no to are the dates given by the claimant correct
+  And I enter invalid dates in the incorrect format
+  Then I should still be on the claimant details page and not on the earnings and benefits page
+
+
+
--- features/factories/et3_claimant_factory.rb
+++ features/factories/et3_claimant_factory.rb
@@ -30,6 +30,32 @@ FactoryBot.define do
     disagree_employment {"lorem ipsum employment"}
   end
 
+  trait :disagree_with_employment_dates_invalid_date do
+    claimants_name { Faker::Name.name }
+    agree_with_early_conciliation_details {:"questions.agree_with_early_conciliation_details.options.no"}
+    disagree_conciliation_reason {"lorem ipsum conciliation"}
+    continued_employment {:"questions.continued_employment.options.no"}
+    agree_with_claimants_description_of_job_or_title {:"questions.agree_with_claimants_description_of_job_or_title.options.no"}
+    disagree_claimants_job_or_title {"lorem ipsum job title"}
+    agree_with_claimants_hours {:"questions.agree_with_claimants_hours..options.no"}
+    queried_hours {32.0}
+    agree_with_earnings_details {:"questions.agree_with_earnings_details.options.no"}
+    queried_pay_before_tax {1000.0}
+    queried_pay_before_tax_period {"Monthly"}
+    queried_take_home_pay {900.0}
+    queried_take_home_pay_period {"Monthly"}
+    agree_with_claimant_notice {:"questions.agree_with_claimant_notice.options.no"}
+    disagree_claimant_notice_reason {"lorem ipsum notice reason"}
+    agree_with_claimant_pension_benefits {:"questions.agree_with_claimant_pension_benefits.options.no"}
+    disagree_claimant_pension_benefits_reason {"lorem ipsum claimant pension"}
+    defend_claim {:"questions.defend_claim.options.yes"}
+    defend_claim_facts {"lorem ipsum defence"}
+    agree_with_employment_dates {:"questions.agree_with_employment_dates.options.no"}
+    employment_start {"0/0/0"}
+    employment_end {"0/0/0"}
+    disagree_employment {"lorem ipsum employment"}
+  end
+
   trait :agree_with_employment_dates do
     claimants_name { Faker::Name.name }
     agree_with_early_conciliation_details {:"questions.agree_with_early_conciliation_details.options.no"}
